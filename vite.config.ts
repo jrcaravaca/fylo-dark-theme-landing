76ac112850fb9e5f41071f4ca077f145a48247f0
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-01-12 06:57:47 +0100

    settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
-export default defineConfig({
-  base:'/Fylo-dark-theme-landing/',
-  plugins: [
-    react(), 
-    tailwindcss()],
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production'
+    ? '/Fylo-dark-theme-landing/'
+    : '/',
+  plugins: [react(), tailwindcss()],
+}))
